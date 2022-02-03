3adf7c63f3cedcba9f43a801f62db7d2ff8edd62
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-02-03 10:57:18 +0530

    Create sliding_window_median.cpp

---
+++ data-structures/sliding_window_median.cpp
@@ -0,0 +1,34 @@
+// TODO : Check how the median is defined
+template<typename T>
+struct slmed{
+	multiset<T> bef,aft;
+	slmed(){}
+	void fix(){
+		if(bef.size()){
+			auto itr = bef.end(); itr--;
+			aft.insert(*itr); bef.erase(itr);
+		}
+		while(bef.size() < aft.size()){
+			bef.insert(*aft.begin()); aft.erase(aft.begin());
+		}
+		
+	}
+	void insert(T x){
+		bef.insert(x);
+		fix();
+	}
+	void erase(T x){ // Can't remove if not present. Will throw error / TLE
+		auto itr = bef.end(); itr--;
+		if(*itr < x) aft.erase(aft.lower_bound(x));
+		else bef.erase(bef.lower_bound(x));
+		fix();
+	}
+	T tell(){
+		auto itr = bef.end(); itr--;
+		if(bef.size() == aft.size() + 1)return *itr;
+		return (*itr + *aft.begin())/2.0;
+	}
+	void clear(){
+		bef.clear();aft.clear();
+	}
+};
