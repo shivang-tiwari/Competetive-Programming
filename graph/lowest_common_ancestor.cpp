1a72d50b27547f3852dbf59c9e855b9f9878dd30
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-02-04 11:55:38 +0530

    Update lowest_common_ancestor.cpp

--- graph/lowest_common_ancestor.cpp
+++ graph/lowest_common_ancestor.cpp
@@ -1,63 +1,62 @@
-struct LCA {
-	vector<int> height, euler, first, segtree;
-	vector<bool> visited;
-	int n;
-
-	LCA(vector<vector<int>> &adj, int root = 0) {
-		n = adj.size();
-		height.resize(n);
-		first.resize(n);
-		euler.reserve(n * 2);
-		visited.assign(n, false);
-		dfs(adj, root);
-		int m = euler.size();
-		segtree.resize(m * 4);
-		build(1, 0, m - 1);
-	}
-
-	void dfs(vector<vector<int>> &adj, int node, int h = 0) {
-		visited[node] = true;
-		height[node] = h;
-		first[node] = euler.size();
-		euler.push_back(node);
-		for (auto to : adj[node]) {
-			if (!visited[to]) {
-				dfs(adj, to, h + 1);
-				euler.push_back(node);
+class LCA{
+	template<typename T>
+	struct SparseTable{
+		vector<vector<T>> table;
+		vector<int> logtable;
+		function<T(T, T)> merge_func;
+		SparseTable(){}
+		SparseTable(const vector<T> &vec, const function<T(T, T)> &f): merge_func(f){
+			int maxlength = 0;
+			while((1 << (maxlength+1)) <= (int)vec.size()) maxlength++;
+			table.resize(maxlength+1, vector<T>(vec.size()));
+			logtable.resize(vec.size()+1);
+			for(int i = 0;i < maxlength + 1;i++){
+				for(int j = 0;j < (int)vec.size() - (1 << i) + 1;j++){
+					if(i)table[i][j] = merge_func(table[i-1][j], table[i-1][j + (1 << (i-1))]);
+					else table[i][j] = vec[j];
+				}
 			}
+			for(int i = 2;i <= (int)vec.size();i++)logtable[i] = logtable[i >> 1]+1;
 		}
-	}
-
-	void build(int node, int b, int e) {
-		if (b == e) {
-			segtree[node] = euler[b];
-		} else {
-			int mid = (b + e) / 2;
-			build(node << 1, b, mid);
-			build(node << 1 | 1, mid + 1, e);
-			int l = segtree[node << 1], r = segtree[node << 1 | 1];
-			segtree[node] = (height[l] < height[r]) ? l : r;
+		T query(int l, int r){
+			assert(l < r);
+			int length = r - l;
+			return merge_func(table[logtable[length]][l], table[logtable[length]][r - (1 << logtable[length])]);
 		}
+	};
+	private:
+	int a = 0, b = 0;
+	vector<int> begin, number, tour, dep, rev;
+	SparseTable<int> table;
+	void init_dfs(int v, int p, const vector<vector<int>> &g){
+		number[v] = b;rev[b] = v;b++;
+		for(auto t : g[v]){
+			if(t == p) continue;
+			dep[t] = dep[v] + 1;
+			init_dfs(t, v, g);
+			tour.push_back(number[v]);
+			if(begin[v] == -1)begin[v] = a;
+			a++;
+		}
+		if(begin[v] == -1)begin[v] = a;
 	}
-
-	int query(int node, int b, int e, int L, int R) {
-		if (b > R || e < L)
-			return -1;
-		if (b >= L && e <= R)
-			return segtree[node];
-		int mid = (b + e) >> 1;
-
-		int left = query(node << 1, b, mid, L, R);
-		int right = query(node << 1 | 1, mid + 1, e, L, R);
-		if (left == -1) return right;
-		if (right == -1) return left;
-		return height[left] < height[right] ? left : right;
+	public:
+	//initialization O(NlogN)
+	LCA(const vector<vector<int>> &g, int root = 0):begin(g.size(), -1), number(g.size()), dep(g.size(), -1), rev(g.size()){
+		dep[root] = 0;
+		init_dfs(root, -1, g);
+		table = SparseTable<int>(tour, [](int x, int y){return min(x, y);});
 	}
-
-	int lca(int u, int v) {
-		int left = first[u], right = first[v];
-		if (left > right)
-			swap(left, right);
-		return query(1, 0, euler.size() - 1, left, right);
+	//O(1) per query
+	int lca(int u, int v){
+		if(begin[u] == begin[v]) return (dep[u] > dep[v] ? v : u);
+		if(begin[u] > begin[v]) swap(u, v);
+			return rev[table.query(begin[u], begin[v]+1)];
+	}
+	int depth(int v){
+		return dep[v];
+	}
+	int dist(int u, int v){
+		return dep[u] + dep[v] - 2*dep[lca(u, v)];
 	}
 };
