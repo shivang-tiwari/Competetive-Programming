d13957b2c26f4863422e9fbed536a91794bbd41c
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-02-09 10:33:13 +0530

    Update matrix.cpp

--- math/matrix.cpp
+++ math/matrix.cpp
@@ -1,28 +1,29 @@
-template <typename T>
-vector<vector<T>> multiply(vector<vector<T>> a, vector<vector<T>> b){
-	assert(a[0].size() == b.size());
-	int r1 = a.size(),c1 = a[0].size(),c2 = b[0].size();
-	vector<vector<T>> res(r1,vector<T>(c1,0));
-	for (int i = 0; i < r1; i++){
-		for (int j = 0; j < c2; j++){
-			for (int k = 0; k < c1; k++)
-				res[i][j] += a[i][k]*b[k][j];
+template<typename T>
+vector<vector<T>> multiply(const vector<vector<T>> &a, const vector<vector<T>> &b) {
+	int N = a.size(), M = a[0].size(), K = b[0].size();
+	assert(M == (int)b.size());
+	vector<vector<T>> c(N,vector<T>(K));
+	for(int i = 0; i < N; i++) {
+		for(int k = 0; k < M; k++) {
+			for(int j = 0; j < K; j++) {
+				c[i][j] += a[i][k] * b[k][j];
+			}
 		}
 	}
-	return res;
+	return c;
 }
 
-template <typename T>vector<T> multiply(vector<vector<T>> a, vector<T> b){return multiply({b},a)[0];}
 
-template <typename T>
-vector<vector<T>> power(vector<vector<T>> x, int y){
+template <typename T, typename U>
+vector<vector<T>> power(const vector<vector<T>> &x, U y){
 	int n = x.size();
+	assert(n == (int)x[0].size());
 	vector<vector<T>> res(n,vector<T>(n));
 	for(int i = 0; i < n; i++){
 		res[i][i] = 1;
 	}
-	while (y > 0){
-		if (y & 1)
+	while(y > 0){
+		if(y & 1)
 			res = multiply(res,x);
 		y = y >> 1;
 		x = multiply(x,x);
