010aff1990af31064d1f7c421a5d0a3004366228
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-06-05 18:28:06 +0530

    Update dijkstra.cpp

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,17 +1,35 @@
-int n,m;
-vector<vector<pair<int,int>>> graph; // id,weight
-vector<int> dist,pre;
-void dijkstra(int start){
-	dist.clear();dist.resize(n,1e18);
-	pre.clear();pre.resize(n,-1);
-	priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> que;
+vector<long long int> dist;
+vector<int> pre;
+void dijkstra(int start,vector<vector<pair<int,int>>> &adj){
+	dist.clear();dist.resize(adj.size(),1e18);
+	pre.clear();pre.resize(adj.size(),-1);
+	priority_queue<pair<long long int,int>,vector<pair<long long int,int>>,greater<pair<long long int,int>>> que;
 	que.push({dist[start] = 0, start});
 	while (!que.empty()) {
 		auto top = que.top();
 		que.pop();
 		if (top.first != dist[top.second]) continue;
-		for(auto node : graph[top.second])
+		for(auto node : adj[top.second])
 			if(dist[node.first] > top.first + node.second) 
 				que.push({dist[node.first] = top.first + node.second, node.first}), pre[node.first] = top.second;
 	}
 }
+
+/*  No weights
+vector<int> pre;
+vector<long long int>dist;
+void dijkstra(int start,vector<vector<int>> &adj){
+	dist.clear();dist.resize(adj.size(),1e18);
+	pre.clear();pre.resize(adj.size(),-1);
+	queue<pair<int,int>> que;
+	que.push({dist[start] = 0, start});
+	while (!que.empty()) {
+		auto top = que.front();
+		que.pop();
+		if (top.first != dist[top.second]) continue;
+		for(auto node : adj[top.second])
+			if(dist[node] > top.first + 1) 
+				que.push({dist[node] = top.first + 1, node}), pre[node] = top.second;
+	}
+}
+*/
