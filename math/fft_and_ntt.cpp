60ae15dc460680e40663dae4041723fc1d374714
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-08-30 12:41:31 +0530

    Update fft_and_ntt.cpp

--- math/fft_and_ntt.cpp
+++ math/fft_and_ntt.cpp
@@ -1,106 +1,78 @@
-const int mod = 998244353;
+// ------------------------------------- FFT --------------------------------------------- //
 
-struct base {
-	double x, y;
-	base(){
-		x = y = 0;
-	}
-	base(double a, double b){
-		x = a;
-		y = b;
-	}
-};
-inline base operator + (base a, base b) { return base(a.x + b.x, a.y + b.y); }
-inline base operator - (base a, base b) { return base(a.x - b.x, a.y - b.y); }
-inline base operator * (base a, base b) { return base(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x); }
-inline base conj(base a) { return base(a.x, -a.y); }
-int lim = 1;
-vector<base> roots = {{0, 0}, {1, 0}};
-vector<int> rev = {0, 1};
-const double PI = acosl(- 1.0);
-void ensure_base(int p){
-	if(p <= lim) return;
-	rev.resize(1 << p);
-	for(int i = 0; i < (1 << p); i++)
-		rev[i] = (rev[i >> 1] >> 1) + ((i & 1)  <<  (p - 1));
-	roots.resize(1 << p);
-	while(lim < p) {
-		double angle = 2 * PI / (1 << (lim + 1));
-		for(int i = 1 << (lim - 1); i < (1 << lim); i++){
-			roots[i << 1] = roots[i];
-			double angle_i = angle * (2 * i + 1 - (1 << lim));
-			roots[(i << 1) + 1] = base(cos(angle_i), sin(angle_i));
-		}
-		lim++;
-	}
+using cd = complex<double>;
+const double PI = acos(-1);
+
+int reverse(int num, int lg_n) {
+    int res = 0;
+    for (int i = 0; i < lg_n; i++) {
+        if (num & (1 << i))
+            res |= 1 << (lg_n - 1 - i);
+    }
+    return res;
 }
-void fft(vector<base> &a, int n = -1) {
-	if(n == -1) n = a.size();
-	assert((n & (n - 1)) == 0);
-	int zeros = __builtin_ctz(n);
-	ensure_base(zeros);
-	int shift = lim - zeros;
-	for(int i = 0; i < n; i++)if(i < (rev[i] >> shift)) swap(a[i], a[rev[i] >> shift]);
-	for(int k = 1; k < n; k <<= 1) {
-		for(int i = 0; i < n; i += 2 * k) {
-			for(int j = 0; j < k; j++) {
-				base z = a[i + j + k] * roots[j + k];
-				a[i + j + k] = a[i + j] - z;a[i + j] = a[i + j] + z;
+
+void fft(vector<cd> & a, bool invert) {
+	int n = a.size();
+	int lg_n = 0;
+	while ((1 << lg_n) < n)
+		lg_n++;
+	for(int i = 0;i < n;i++) {
+		if (i < reverse(i, lg_n))
+			swap(a[i], a[reverse(i, lg_n)]);
+	}
+	for(int len = 2;len <= n;len <<= 1){
+		double ang = 2 * PI / len * (invert ? -1 : 1);
+		cd wlen(cos(ang), sin(ang));
+		for (int i = 0;i < n;i += len){
+			cd w(1);
+			for (int j = 0; j < len / 2; j++) {
+				cd u = a[i+j], v = a[i+j+len/2] * w;
+				a[i+j] = u + v;
+				a[i+j+len/2] = u - v;
+				w *= wlen;
 			}
 		}
 	}
-}
-vector<int> multiply(vector<int> &a, vector<int> &b, bool eq = 0){
-	int need = a.size() + b.size() - 1;
-	int p = 0;
-	while((1 << p) < need)p++;
-	ensure_base(p);
-	int sz = 1 << p;
-	vector<base> A, B;
-	if(sz > (int)A.size()) A.resize(sz);
-	for(int i = 0; i < (int)a.size(); i++){
-		int x = (a[i] % mod + mod) % mod;
-		A[i] = base(x & ((1 << 15) - 1), x >> 15);
+	if(invert){
+		for(cd &x : a)
+			x /= n;
 	}
-	fill(A.begin() + a.size(), A.begin() + sz, base{0, 0});
-	fft(A, sz);
-	if(sz > (int)B.size()) B.resize(sz);
-	if(eq) copy(A.begin(), A.begin() + sz, B.begin());
-		else{
-	for(int i = 0; i < (int)b.size(); i++){
-			int x = (b[i] % mod + mod) % mod;
-			B[i] = base(x & ((1 << 15) - 1), x >> 15);
-		}
-		fill(B.begin() + b.size(), B.begin() + sz, base{0, 0});
-		fft(B, sz);
-	}
-	double ratio = 0.25 / sz;
-	base r2(0,  - 1), r3(ratio, 0), r4(0,  - ratio), r5(0, 1);
-	for(int i = 0; i <= (sz >> 1); i++) {
-		int j = (sz - i) & (sz - 1);
-		base a1 = (A[i] + conj(A[j])), a2 = (A[i] - conj(A[j])) * r2;
-		base b1 = (B[i] + conj(B[j])) * r3, b2 = (B[i] - conj(B[j])) * r4;
-		if(i != j) {
-			base c1 = (A[j] + conj(A[i])), c2 = (A[j] - conj(A[i])) * r2;
-			base d1 = (B[j] + conj(B[i])) * r3, d2 = (B[j] - conj(B[i])) * r4;
-			A[i] = c1 * d1 + c2 * d2 * r5;
-			B[i] = c1 * d2 + c2 * d1;
-		}
-		A[j] = a1 * b1 + a2 * b2 * r5;
-		B[j] = a1 * b2 + a2 * b1;
-	}
-	fft(A, sz); fft(B, sz);
-	vector<int> res(need);
-	for(int i = 0; i < need; i++){
-		long long aa = A[i].x + 0.5;
-		long long bb = B[i].x + 0.5;
-		long long cc = A[i].y + 0.5;
-		res[i] = (aa + ((bb % mod) << 15) + ((cc % mod) << 30))%mod;
-	}
-	return res;
 }
 
-// NTT below, needs modular
+
+vector<int> multiply(vector<int> const& a, vector<int> const& b){
+	vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+	int n = 1;
+	while (n < (int)a.size() + (int)b.size()) 
+		n <<= 1;
+	fa.resize(n);
+	fb.resize(n);
+	
+	fft(fa, false);
+	fft(fb, false);
+	for (int i = 0; i < n; i++)
+		fa[i] *= fb[i];
+	fft(fa, true);
+	
+	vector<int> result(n);
+	for (int i = 0; i < n; i++)
+		result[i] = round(fa[i].real());
+	/* 
+	// Use this for long number muliplication. Numbers in reverse order
+	int carry = 0;
+	for (int i = 0; i < n; i++){
+		result[i] += carry;
+		carry = result[i] / 10;
+		result[i] %= 10;
+	}
+	*/
+	
+	return result;
+}
+
+
+// ------------------------------------- NTT --------------------------------------------- //
 template <typename T>
 class NTT {
  public:
