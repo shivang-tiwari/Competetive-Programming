94f79013ab083d4dd5e80698cfc346db256bef15
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-09-26 16:30:24 +0530

    Update articulation_points.cpp

--- graph/articulation_points.cpp
+++ graph/articulation_points.cpp
@@ -14,17 +14,17 @@ vector<int> find_cutpoints(const vector<vector<int>> &adj) {
 		visited[v] = true;
 		tin[v] = low[v] = timer++;
 		int children=0;
-		for (int to : adj[v]) {
-			if (to == p) continue;
-			if (visited[to]) {
+		for (int to : adj[v]){
+			if(to == p) continue;
+			if(visited[to]){
 				low[v] = min(low[v], tin[to]);
-			} else {
+			}
+			else{
 				dfs(to, v);
 				low[v] = min(low[v], low[to]);
-				if (low[to] >= tin[v] && p!=-1){
+				if(low[to] >= tin[v] && p!=-1){
 					// v is an articulation point
 					cutpoints.push_back(v);
-					
 				}
 				++children;
 			}
@@ -34,8 +34,8 @@ vector<int> find_cutpoints(const vector<vector<int>> &adj) {
 			cutpoints.push_back(v);
 		}
 	};
-	for (int i = 0; i < n; ++i) {
-		if (!visited[i])
+	for (int i = 0; i < n; ++i){
+		if(!visited[i])
 			dfs(i,-1);
 	}
 	return cutpoints;
