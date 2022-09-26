a1ee9d0aa7b2d91ad31ed8acde0d44d5a4356955
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-09-26 16:23:33 +0530

    Create bridges.cpp

---
+++ graph/bridges.cpp
@@ -0,0 +1,33 @@
+vector<pair<int,int>> find_bridges(vector<vector<int>> &adj){
+	int n = adj.size();
+	vector<bool> visited;
+	vector<int> tin, low;
+	int timer;
+	timer = 0;
+	visited.assign(n, false);
+	tin.assign(n, -1);
+	low.assign(n, -1);
+	vector<pair<int,int>> bridges;
+	function<void(int,int)> dfs = [&](int v, int p){
+		visited[v] = true;
+		tin[v] = low[v] = timer++;
+		for (int to : adj[v]) {
+			if (to == p) continue;
+			if (visited[to]) {
+				low[v] = min(low[v], tin[to]);
+			} else {
+				dfs(to, v);
+				low[v] = min(low[v], low[to]);
+				if (low[to] > tin[v]){
+					// {v - to} is a bridge
+					bridges.push_back({v,to});
+				}
+			}
+		}
+	};
+	for (int i = 0; i < n; ++i) {
+		if (!visited[i])
+			dfs(i,-1);
+	}
+	return bridges;
+}
