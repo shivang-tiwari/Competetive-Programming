160f865357a06534e1ec3bbba2c6cfcf8c6d30f1
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-11-14 13:29:40 +0530

    Update matrix.cpp

--- math/matrix.cpp
+++ math/matrix.cpp
@@ -1,32 +1,113 @@
 template<typename T>
-vector<vector<T>> multiply(const vector<vector<T>> &a, const vector<vector<T>> &b) {
-	int N = a.size(), M = a[0].size(), K = b[0].size();
-	assert(M == (int)b.size());
-	vector<vector<T>> c(N,vector<T>(K));
-	for(int i = 0; i < N; i++) {
-		for(int k = 0; k < M; k++) {
-			for(int j = 0; j < K; j++) {
-				c[i][j] += a[i][k] * b[k][j];
+struct matrix{
+	int n;
+	int m;
+	vector<vector<T>> a;
+	
+	matrix(int _n,int _m){
+		n = _n;
+		m = _m;
+		a = vector<vector<T>> (n,vector<T>(m));
+	}
+	
+	matrix operator + (const matrix &oth){
+		assert(n == oth.n && m == oth.m);
+		matrix<T> res;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				res[i][j] = a[i][j] + oth.a[i][j];
+			}
+		}
+		return res;
+	}
+	void operator += (const matrix &oth){
+		assert(n == oth.n && m == oth.m);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				a[i][j] += oth.a[i][j];
 			}
 		}
 	}
-	return c;
-}
+	matrix operator - (const matrix &oth){
+		assert(n == oth.n && m == oth.m);
+		matrix<T> res;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				res[i][j] = a[i][j] - oth.a[i][j];
+			}
+		}
+		return res;
+	}
+	void operator -= (const matrix &oth){
+		assert(n == oth.n && m == oth.m);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				a[i][j] -= oth.a[i][j];
+			}
+		}
+	}
+	matrix operator * (const matrix &oth){
+		assert(m == oth.n);
+		matrix<T> res(n,oth.m);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < oth.m; j++){
+				for(int k = 0; k < m; k++){
+					res[i][j] += a[i][k] * oth.a[k][j];
+				}
+			}
+		}
+		return res;
+	}
+	
+	void operator *= (const matrix &oth){
+		assert(m == oth.n);
+		matrix<T> res(n,oth.m);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < oth.m; j++){
+				for(int k = 0; k < m; k++){
+					res[i][j] += a[i][k] * oth.a[k][j];
+				}
+			}
+		}
+		a = res.a;
+		n = res.n;
+		m = res.m;
+	}
+	
+	
+	// Scalar operations
+	matrix operator * (const int &num){
+		matrix<T> res(n,m);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				res[i][j] = a[i][j] * num;
+			}
+		}
+	}
+	matrix operator *= (const int &num){
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				a[i][j] *= num;
+			}
+		}
+	}
+	vector<T>& operator [] (int i) {return a[i];}
+};
 
-
-template <typename T, typename U>
-vector<vector<T>> power(const vector<vector<T>> &x, U y){
-	int n = x.size();
-	assert(n == (int)x[0].size());
-	vector<vector<T>> res(n,vector<T>(n));
-	for(int i = 0; i < n; i++){
+template<typename T>
+matrix<T> power(const matrix<T> &x, int64_t y){
+	assert(x.n == x.m);
+	matrix<T> res(x.n,x.n);
+	for(int i = 0; i < x.n; i++){
 		res[i][i] = 1;
 	}
+	auto b = x;
 	while(y > 0){
-		if(y & 1)
-			res = multiply(res,x);
+		if(y & 1){
+			res *= b;
+		}
 		y = y >> 1;
-		x = multiply(x,x);
+		b *= b;
 	}
 	return res;
 }
