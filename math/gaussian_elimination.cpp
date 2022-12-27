4aed56ce55b394cc5e5ef06a98a030319799f97a
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-12-27 10:34:41 +0530

    Gaussian elimination for modulo

--- math/gaussian_elimination.cpp
+++ math/gaussian_elimination.cpp
@@ -1,25 +1,32 @@
 const double EPS = 1e-9;
 const int INF = 2; // it doesn't actually have to be infinity or a big number
 
-int gauss (vector<vector<double>> a, vector<double> &ans) { 
+template <typename T>
+int gauss (vector<vector<T>> a, vector<T> &ans) { 
 	int n = (int) a.size();
 	int m = (int) a[0].size() - 1;
-	
 	vector<int> where (m, -1);
 	for(int col = 0, row = 0; col < m && row < n; ++col){
 		int sel = row;
-		for(int i = row; i < n; ++i)
-			if(abs(a[i][col]) > abs(a[sel][col]))
-				sel = i;
-		if(abs(a[sel][col]) < EPS)
-			continue;
+		if(is_same<T,double>::value){
+			for(int i = row; i < n; ++i)
+				if(abs(a[i][col]) > abs(a[sel][col]))
+					sel = i;
+			if(abs(a[sel][col]) < EPS)
+				continue;
+		}
+		else{
+			if(a[sel][col] == 0){
+				continue;
+			}
+		}
 		for(int i = col;i <= m; ++i)
 			swap (a[sel][i], a[row][i]);
 		where[col] = row;
 	
 		for(int i = 0; i < n; ++i){
 			if(i != row) {
-				double c = a[i][col] / a[row][col];
+				T c = a[i][col] / a[row][col];
 				for(int j = col; j <= m; ++j)
 					a[i][j] -= a[row][j] * c;
 			}
@@ -31,18 +38,23 @@ int gauss (vector<vector<double>> a, vector<double> &ans) {
 		if(where[i] != -1)
 			ans[i] = a[where[i]][m] / a[where[i]][i];
 	for(int i = 0; i < n; ++i) {
-		double sum = 0;
+		T sum = 0;
 		for(int j = 0; j < m; ++j)
 			sum += ans[j] * a[i][j];
-		if(abs(sum - a[i][m]) > EPS)
-			return 0;
+		if(is_same<T,double>::value){
+			if(abs(sum - a[i][m]) > EPS)
+				return 0;
+		}
+		else{
+			if(sum != a[i][m]){
+				return 0;
+			}
+		}
 	}
-	
 	for(int i = 0; i < m; ++i)
 		if(where[i] == -1)
 			return INF;
 	return 1;
 }
-
 // solves Ax = B
 // B is the last column of A
