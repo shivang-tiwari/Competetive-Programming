db8787708b8249ec63369c767f02ed0475ad0869
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2023-01-22 10:25:53 +0530

    Added function to build the compressed graph

--- graph/strongly_connected_components.cpp
+++ graph/strongly_connected_components.cpp
@@ -2,12 +2,13 @@ vector<vector<int>> scc;
 vector<int> comp;
 
 void SCC(vector<vector<int>> &adj){
+	scc.clear();comp.clear();
 	int V = adj.size();
 	comp.resize(V);
-	int *disc = new int[V];
-	int *low = new int[V];
-	bool *stackMember = new bool[V];
-	stack<int> *st = new stack<int>();
+	vector<int> disc(V);
+	vector<int> low(V);
+	vector<bool> stackMember(V);
+	stack<int> st;
 	for (int i = 0; i < V; i++){
 		disc[i] = -1;
 		low[i] = -1;
@@ -16,7 +17,7 @@ void SCC(vector<vector<int>> &adj){
 	static int time = 0;
 	function<void(int)> find_scc = [&](int u){
 		disc[u] = low[u] = ++time;
-		st->push(u);
+		st.push(u);
 		stackMember[u] = true;
 		for (auto i = adj[u].begin(); i != adj[u].end(); ++i){
 			int v = *i;
@@ -31,22 +32,37 @@ void SCC(vector<vector<int>> &adj){
 		if (low[u] == disc[u]){
 			vector<int> row;
 			int ID = scc.size();
-			while (st->top() != u){
-				w = (int) st->top();
+			while (st.top() != u){
+				w = (int) st.top();
 				row.push_back(w);
 				comp[w] = ID;
 				stackMember[w] = false;
-				st->pop();
+				st.pop();
 			}
-			w = (int) st->top();
+			w = (int) st.top();
 			row.push_back(w);
 			comp[w] = ID;
 			scc.push_back(row);
 			stackMember[w] = false;
-			st->pop();
+			st.pop();
 		}
 	};
 	for (int i = 0; i < V; i++){
 		if (disc[i] == -1)find_scc(i);
 	}
+}
+
+// Call this immediately after SCC to get the compressed graph
+
+vector<vector<int>> build_graph(const vector<vector<int>> &adj){ 
+	int n = adj.size();
+	vector<vector<int>> scc_graph(scc.size());
+	for(int i = 0; i < n; i++){
+		for(int x : adj[i]){
+			if(comp[i] != comp[x]){
+				scc_graph[comp[i]].push_back(comp[x]);
+			}
+		}
+	}
+	return scc_graph;
 }
