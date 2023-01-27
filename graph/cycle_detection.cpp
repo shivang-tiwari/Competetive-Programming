de39aaf4dbabe2005f9f190a2a6e86c4329b272e
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2023-01-27 17:13:23 +0530

    Update cycle_detection.cpp

--- graph/cycle_detection.cpp
+++ graph/cycle_detection.cpp
@@ -1,43 +1,13 @@
-//This detects cycle in a ***directed graph***
+//This detects cycle in an ***undirected graph***
+vector<int> cyc;
 bool isCyclic(vector<vector<int>> &adj){
 	int V = adj.size();
 	vector<bool> visited(V);
-	vector<bool> recStack(V);
-	for(int i = 0; i < V; i++){
-		visited[i] = false;
-		recStack[i] = false;
-	}
-	function<bool(int)> cycle = [&](int v){
-			if(visited[v] == false){
-				visited[v] = true;
-				recStack[v] = true;
-				for(int i : adj[v]){
-					if (!visited[i] && cycle(i))return true;
-					else if (recStack[i])return true;
-				}
-			}
-			recStack[v] = false;
-			return false;
-	};
-	for(int i = 0; i < V; i++){
-		if(cycle(i))return true;
-	}
-	return false;
-}
-
-//This detects cycle in an ***undirected graph***
-vector<int> cyc;
-bool isCyclic(vector<vector<int>> &adj,bool need_cycle = false){
-	int V = adj.size();
-	cyc.clear();
-	bool *visited = new bool[V];
 	for(int i = 0; i < V; i++){
 		visited[i] = false;
 	}
 	function<bool(int,int)> cycle = [&](int v,int parent) -> bool {
-		if(need_cycle){
-			cyc.push_back(v);
-		}
+		cyc.push_back(v);
 		visited[v] = true;
 		for(int i : adj[v]){
 			if (!visited[i]){
@@ -46,17 +16,13 @@ bool isCyclic(vector<vector<int>> &adj,bool need_cycle = false){
 				}
 			}
 			else if(i != parent){
-				if(need_cycle){
-					reverse(cyc.begin(),cyc.end());
-					while(!cyc.empty() && cyc.back() != i)cyc.pop_back();
-					reverse(cyc.begin(),cyc.end());
-				}
+				reverse(cyc.begin(),cyc.end());
+				while(!cyc.empty() && cyc.back() != i)cyc.pop_back();
+				reverse(cyc.begin(),cyc.end());
 				return true;
 			}
 		}
-		if(need_cycle){
-			cyc.pop_back();
-		}
+		cyc.pop_back();
 		return false;
 	};
 	for(int i = 0; i < V; i++){
