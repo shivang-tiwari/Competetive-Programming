5c963198fb797c9f01e6ee5b4b32a61bc327bdbf
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2023-01-27 17:14:04 +0530

    Update cycle_detection.cpp

--- graph/cycle_detection.cpp
+++ graph/cycle_detection.cpp
@@ -1,3 +1,31 @@
+//This detects cycle in a ***directed graph***
+bool isCyclic(vector<vector<int>> &adj){
+	int V = adj.size();
+	vector<bool> visited(V);
+	vector<bool> recStack(V);
+	for(int i = 0; i < V; i++){
+		visited[i] = false;
+		recStack[i] = false;
+	}
+	function<bool(int)> cycle = [&](int v){
+			if(visited[v] == false){
+				visited[v] = true;
+				recStack[v] = true;
+				for(int i : adj[v]){
+					if (!visited[i] && cycle(i))return true;
+					else if (recStack[i])return true;
+				}
+			}
+			recStack[v] = false;
+			return false;
+	};
+	for(int i = 0; i < V; i++){
+		if(cycle(i))return true;
+	}
+	return false;
+}
+
+
 //This detects cycle in an ***undirected graph***
 vector<int> cyc;
 bool isCyclic(vector<vector<int>> &adj){
