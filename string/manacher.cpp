a721e26e13ae3568ad0fa09b6e8f0d6dcf22b8ef
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2026-02-15 13:58:20 +0800

    Refactoring manacher in a class

--- string/manacher.cpp
+++ string/manacher.cpp
@@ -1,35 +1,56 @@
-vector<int> manacher_odd(string s){
-	int n = s.size();
-	s = "$" + s + "^";
-	vector<int> p(n + 2);
-	int l = 1, r = 1;
-	for(int i = 1; i <= n; i++) {
-		p[i] = max(0, min(r - i, p[l + (r - i)]));
-		while(s[i - p[i]] == s[i + p[i]]) {
-			p[i]++;
+class Manacher{
+	public:
+	
+	vector<int> d1;
+	vector<int> d2;
+	void manacher(const string &s){
+		int n = s.size();
+		if((int)t.size() != 2 * n + 1 + 2){
+			t.resize(2 * n + 1 + 2);
 		}
-		if(i + p[i] > r) {
-			l = i - p[i], r = i + p[i];
+		t[0] = '$';
+		for(int i = 0; i < n; i++){
+			t[2*i+1] = '#';
+			t[2*i+2] = s[i];
+		}
+		t[2*n+1] = '#';
+		t[2*n+2] = '^';
+		span<int> res = manacher_odd(t);
+		res = span<int>(begin(res) + 1, end(res) - 1);
+		d1.resize(s.size());
+		d2.resize(s.size());
+		for(int i = 0; i < (int)s.size(); i++){
+			d1[i] = res[2*i] / 2;
+			if(i){
+				d2[i] = (res[2*i - 1] - 1) / 2;
+			}
 		}
 	}
-	return vector<int>(begin(p) + 1, end(p) - 1);
-}
-pair<vector<int>,vector<int>> manacher(string s){
+	
+	
+	protected:
+	
 	string t;
-	for(auto c: s) {
-		t += string("#") + c;
-	}
-	auto res = manacher_odd(t + "#");
-	res = vector<int>(begin(res) + 1, end(res) - 1);
-	vector<int> d1(s.size()),d2(s.size());
-	for(int i = 0; i < (int)s.size(); i++){
-		d1[i] = res[2*i] / 2;
-		if(i){
-			d2[i] = (res[2*i - 1] - 1) / 2;
+	vector<int> p;
+	
+	span<int> manacher_odd(const string &s){
+		int n = s.size() - 2;
+		if((int)p.size() != n+2){
+			p.resize(n+2);
 		}
+		int l = 1, r = 1;
+		for(int i = 1; i <= n; i++) {
+			p[i] = max(0, min(r - i, p[l + (r - i)]));
+			while(s[i - p[i]] == s[i + p[i]]) {
+				p[i]++;
+			}
+			if(i + p[i] > r) {
+				l = i - p[i], r = i + p[i];
+			}
+		}
+		return span<int>(begin(p) + 1, end(p) - 1);
 	}
-	return {d1,d2};
-}
+};
 /*
 d1[i] = number of odd-length palindromes centered at i
 d2[i] = number of even-length palindromes centered at (i-1,i)
